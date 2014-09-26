97ec464897b8343846d84d1037fa4ccf81231d47
Kakarot <JamesRuiz@gatech.edu>
2014-09-26 04:26:06 -0400

    Finished AttackMove class.

--- OneRound/AttackMove.cpp
+++ OneRound/AttackMove.cpp
@@ -1,11 +1,22 @@
 #include "AttackMove.h"
 
 
-AttackMove::AttackMove()
+AttackMove::AttackMove(CString name, int damage)
 {
+	AttackMove::name = name;
+	AttackMove::damage = damage;
+
 }
 
 
 AttackMove::~AttackMove()
 {
+	//consider the case of freeing the CString name.
+}
+CString AttackMove::getName(){
+	return AttackMove::name;
+}
+
+int AttackMove::getDamage(){
+	return AttackMove::damage;
 }
