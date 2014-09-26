8fdc6af02fc7f5b28b826a8b6fca14daa47cc301
Kakarot <JamesRuiz@gatech.edu>
2014-09-26 04:12:42 -0400

    Added CStrings

--- OneRound/AttackMove.h
+++ OneRound/AttackMove.h
@@ -1,11 +1,15 @@
 #pragma once
 #include<string.h>
+#include<atlstr.h>
 class AttackMove
 {
 public:
 	AttackMove();
 	~AttackMove();
+	CString getName();
+	int getDamage();
 private:
 	int damage;
+	CString name;
 };
 
