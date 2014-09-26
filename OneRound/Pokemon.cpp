4d07c608f6eff55455a98d9d5171531d8711b0ef
Kakarot <JamesRuiz@gatech.edu>
2014-09-26 05:05:57 -0400

    Enums

--- OneRound/Pokemon.cpp
+++ OneRound/Pokemon.cpp
@@ -2,11 +2,30 @@
 #include "Pokemon.h"
 
 
-Pokemon::Pokemon()
+Pokemon::Pokemon(CString name, int health, int level, aspect_t pokemonsAspect)
 {
+	Pokemon::name = name;
+	Pokemon::health = health;
+	Pokemon::level = level;
+	Pokemon::pokemonsAspect = pokemonsAspect;
 }
 
 
 Pokemon::~Pokemon()
 {
+}
+CString Pokemon::getName(){
+	return Pokemon::name;
+}
+
+int Pokemon::getHealth(){
+	return Pokemon::health;
+}
+
+int Pokemon::getLevel(){
+	return Pokemon::level;
+}
+
+Pokemon::aspect_t Pokemon::getAspect(){
+	return Pokemon::pokemonsAspect;
 }
